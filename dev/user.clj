a4d25033680b4e791276b5856b8237cdccece249
Teodor Heggelund <git@teod.eu>
2025-07-23 10:25:32 +0300

    Start *actually* translating Broch README

--- dev/user.clj
+++ dev/user.clj
@@ -1,4 +1,10 @@
 (ns user
-  (:require munit.runtime))
+  (:require [clojure.repl.deps :refer [sync-deps add-lib]]
+            munit.runtime))
 
 (alter-var-root #'munit.runtime/dev? (constantly true))
+
+(comment
+  (add-lib 'no.anteo/broch)
+  (sync-deps)
+  )
--- doc/from_broch_readme.clj
+++ doc/from_broch_readme.clj
@@ -1,31 +1,20 @@
 ;; # μnit examples, via [Broch's README](https://github.com/anteoas/broch/blob/66ead26e041c35907c59aea4e4237553ece47aaf/README.md)
-;;
-;; Intended to be viewed with Clerk.
 
 (ns from-broch-readme
   {:nextjournal.clerk/toc true}
   (:refer-clojure :exclude [* / + -])
   (:require
+   [broch.core :as b]
    [munit.convert :as convert]
    [munit.si :refer [m]]
    [munit.units :refer [*]]
    [nextjournal.clerk :as clerk]))
 
-;; We exclude Clojure's artithmetic operators, since we're using our own.
-
 ;; ## Ten meters
+;; ### with broch
 
-^{:nextjournal.clerk/visibility {:code :hide}}
-(clerk/caption
- "with Broch"
- (clerk/code '(b/meters 10)))
+(b/meters 10)
 
-;; To make this work, I need:
-;; - multiplication - 10 * meters
-;; - coercion - coerce 10 and BaseUnit to Quantity
-;; - simplification - to present the result.
+;; ### with munit
 
-;; Requirements:
-;; - coerce BaseUnit to Quanity
-;; - coerce number to Quanitty
-;; - Simplify Quantity to vector of multiplication parts
+(* 10 m)
